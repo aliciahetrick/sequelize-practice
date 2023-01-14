954da2cf5435b245f95529d82fde6b352cfd6e64
aliciahetrick <aliciahetrick@gmail.com>
2023-01-14 16:04:39 -0500

    findAll users

--- services/user-services.js
+++ services/user-services.js
@@ -1,3 +1,23 @@
+// creating a user
+// const User = require('../models/index')['User']
+// class UserService {
+//   async createUser(user) {
+//     return await User.create(user)
+//   }
+// }
+// let user = {
+//   firstName: 'Alicia',
+//   lastName: 'Hetrick',
+//   age: 29,
+// }
+// let userService = new UserService()
+// const result = async () => {
+//   const data = await userService.createUser(user)
+//   console.log(data)
+// }
+// result()
+
+//finding all users
 const User = require('../models/index')['User']
 
 class UserService {
