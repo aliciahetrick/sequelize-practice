599e3bef4933855ed516fa43daf245326187be79
aliciahetrick <aliciahetrick@gmail.com>
2023-01-14 15:24:01 -0500

    Created a single user

--- services/user-services.js
+++ services/user-services.js
@@ -1,2 +1,17 @@
 const User = require('../models/index')['User']
-class UserService {}
+class UserService {
+  async createUser(user) {
+    return await User.create(user)
+  }
+}
+let user = {
+  firstName: 'Alicia',
+  lastName: 'Hetrick',
+  age: 29,
+}
+let userService = new UserService()
+const result = async () => {
+  const data = await userService.createUser(user)
+  console.log(data)
+}
+result()
